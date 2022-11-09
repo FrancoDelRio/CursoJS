d53333b7cb250931961e0ea51c6fc78d00010ada
Franco <francodelrio10@gmail.com>
2022-11-09 10:54:20 -0600

    Explicamos como funciona la palabra reservada para constantes

--- variables/Varieables.js
+++ variables/Varieables.js
@@ -1,5 +1,9 @@
 //let palabra reservada para declarar variables que estan sujetas al scope y no pueden ser redeclaradas 
 //Esto para evitar que una palabra se use para variables diferentes "en el mismo scope"
+let x;
+/*Si no se inicializa una variable let se  */
+console.log(x);
+
 let variable = "hola mundo";
 
 console.log(variable);
@@ -23,4 +27,28 @@ if (true){
 }
 
 console.log(variableVar);
+
+//const es la palabra reservada para declarar constatnes, osea que nose les cambiara el valor nunca
+//*estas variables tienen que ser inicializadas en cuanto se declaran, no se pueden quedar vacias*
+const IVA = 16;
+
+//Las constantes respetan el scope lo que significa que pueden volver a ser declaradas en un scope
+if (true){
+  const IVA = 15.75;
+  console.log(IVA);
+}
+
+console.log(IVA);
+
+//Tambien se pueden cambiar los elementos dentro de un objeto aunque sea constante
+const persona ={
+  nombre: "Franco",
+  apellido: "del Rio"
+}
+
+console.log(persona);
+
+persona.nombre = "Alex";
+
+console.log(persona);
 
