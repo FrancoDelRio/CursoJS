9ba0002291193828f720a27bb2e2d59900361857
Franco <francodelrio10@gmail.com>
2022-11-09 10:08:00 -0600

    Se añade al tema de variables, la diferencia entre var y let

--- variables/Varieables.js
+++ variables/Varieables.js
@@ -1,3 +1,5 @@
+//let palabra reservada para declarar variables que estan sujetas al scope y no pueden ser redeclaradas 
+//Esto para evitar que una palabra se use para variables diferentes "en el mismo scope"
 let variable = "hola mundo";
 
 console.log(variable);
@@ -6,9 +8,19 @@ let numero = 5;
 
 if (numero === 5){
   let numero = 6;
-  console.log(numero);
+  //console.log(numero);
 }
 
 numero = 8;
+//console.log(numero);
 
-console.log(numero);
+
+//var es la palabra clave que no esta sujeta a scopes como tal y puede ser redeclarada las veces que quieras
+var variableVar = "Adios";
+
+if (true){
+  var variableVar ="Hasta luego";
+}
+
+console.log(variableVar);
+
