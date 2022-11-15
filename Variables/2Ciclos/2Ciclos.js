1236a94f92b5cb6f8de66a862591f69df4c2fc7d
Franco <francodelrio10@gmail.com>
2022-11-15 15:58:54 -0600

    se agrego orden en los archivos de JS y se agregaron temas basicos de hmtl

--- Variables/2Ciclos/2Ciclos.js
+++ Variables/2Ciclos/2Ciclos.js
@@ -1,13 +1,12 @@
-
 //El var como no respeta scopes se imprimira 11 ya que es donde el ciclo termina
-for (var i=0; i<=10; i++){
+for (var i = 0; i <= 10; i++) {
 
 }
 console.log(i);
 
 
 //Para evitar que i se quede globalmente con el numero del ciclo se prefiere usar mejor let
-for (let i = 0; i<=5; i++){
+for (let i = 0; i <= 5; i++) {
 
 }
 console.log(i);
